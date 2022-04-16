d02caddf4196f67c3c49c9810d5e24b1e24209cf
Daniel <danielpietruszynski@wp.pl>
2022-04-16 17:18:24 +0200

    adding letters

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 
+const wordle = 'SUPER'
 const keys = [
     'Q',
     'W',
@@ -41,6 +42,9 @@ const guessRows = [
     ['', '', '', '', '']
 ]
 
+let currentRow = 0
+let currentTile = 0
+
 guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div')
     rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
@@ -53,13 +57,34 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     tileDisplay.append(rowElement)
 })
 
-const handleClick = () => {
-    console.log('clicked')
-}
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key
     buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', handleClick)
+    buttonElement.addEventListener('click', () => handleClick(key))
     keyboard.append(buttonElement)
 })
+
+const handleClick = (letter) => {
+    console.log('clicked', letter)
+    if (letter === '«') {
+        console.log('delete letter')
+        return
+    }
+    if (letter === 'ENTER') {
+        console.log('check row')
+        return
+    }
+    addLetter(letter)
+}
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = letter
+        guessRows[currentRow][currentTile] = letter
+        tile.setAttribute('data', letter)
+        currentTile++
+        console.log('guessRows', guessRows)
+    }  
+}
