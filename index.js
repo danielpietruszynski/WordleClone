b370a783305c7cbe4784bcf0896806c899ef05a2
Daniel <danielpietruszynski@wp.pl>
2022-04-16 19:17:36 +0200

    backend v2

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const PORT = 8000
 const axios = require("axios");
 const express = require("express")
+const cors = require("cors")
+require('dotenv').config()
 
 const app = express()
 
+app.use(cors())
+
 app.get('/word', (req, res) => {
-    
     const options = {
         method: 'GET',
         url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
         params: {count: '5', wordLength: '5'},
         headers: {
-          'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
-          'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY
         }
-      };
-      
-      axios.request(options).then((response) => {
-          console.log(response.data);
-      }).catch((error) => {
-          console.error(error);
-      })
+    }
+
+    axios.request(options).then(function (response) {
+	    console.log(response.data);
+        res.json(response.data[0])
+    }).catch(function (error) {
+	    console.error(error);
+    })
 })
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
